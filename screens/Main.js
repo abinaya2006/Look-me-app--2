43a30532bcaf514336856d2a068e800d81e93d2c
abinaya2006 <72507845+abinaya2006@users.noreply.github.com>
2022-11-15 23:05:15 +0530

    Add files via upload

---
+++ screens/Main.js
@@ -0,0 +1,180 @@
+import React from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  StatusBar,
+  Platform,
+  ScrollView,
+  TouchableOpacity,
+  Image
+} from 'react-native';
+
+import * as Permissions from "expo-permissions";
+import * as FaceDetector from 'expo-face-detector';
+import { Camera } from 'expo-camera';
+
+import Filter1 from '../components/Filter1'
+
+
+
+
+export default class Main extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      hasCameraPermission: null,
+      faces: [],
+    }
+    this.onCameraPermission = this.onCameraPermission.bind(this)
+    this.onFacesDetected = this.onFacesDetected.bind(this)
+    this.onFaceDetectionError = this.onFaceDetectionError.bind(this)
+  }
+
+  componentDidMount() {
+    Permissions
+      .askAsync(Permissions.CAMERA)
+      .then(this.onCameraPermission)
+  }
+
+  onCameraPermission({ status }) {
+    this.setState({ hasCameraPermission: status === 'granted' })
+  }
+
+  onFacesDetected({ faces }) {
+    this.setState({ faces: faces })
+  }
+
+  onFaceDetectionError(error) {
+    console.log(error)
+  }
+
+  render() {
+    const { hasCameraPermission } = this.state;
+    if (hasCameraPermission === null) {
+      return <View />
+    }
+    if (hasCameraPermission === false) {
+      return (
+        <View style={styles.container}>
+          <Text>No access to camera</Text>
+        </View>
+      )
+    }
+    console.log(this.state.faces)
+    return (
+      <View style={styles.container}>
+        <SafeAreaView style={styles.droidSafeArea} />
+        <View style={styles.headingContainer}>
+          <View style={{ flexDirection: "row", flexWrap: "wrap" }}>
+            <Text style={styles.titleText1}>FR</Text><Text style={styles.titleText2}>APP</Text>
+
+          </View>
+
+          <View style={{ flexDirection: "row", flexWrap: "wrap" }}>
+            <Text style={styles.subHeading1}>Try Our</Text><Text style={styles.subHeading2}>Cool Frames!</Text>
+          </View>
+        </View>
+        <View style={styles.cameraStyle}>
+          <Camera
+            style={{ flex: 1 }}
+            type={Camera.Constants.Type.front}
+            faceDetectorSettings={{
+              mode: FaceDetector.FaceDetectorMode.accurate,
+              detectLandmarks: FaceDetector.FaceDetectorLandmarks.all,
+              runClassifications: FaceDetector.FaceDetectorClassifications.all
+            }}
+            onFacesDetected={this.onFacesDetected}
+            onFacesDetectionError={this.onFacesDetectionError}
+          />
+          {
+            this.state.faces.map(face => {
+
+              return <Filter1 key={face.faceID} face={face} />
+
+
+            })
+          }
+
+
+        </View>
+
+        <View style={styles.framesContainer}>
+          
+        </View>
+
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+
+  droidSafeArea: {
+    marginTop: Platform.OS === "android" ? StatusBar.currentHeight : 0
+  },
+  headingContainer: {
+    flex: 0.1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: "#6278e4",
+  },
+  titleText1: {
+    // fontSize: RFValue(30),
+    fontWeight: "bold",
+    color: "#efb141",
+    fontStyle: "italic",
+    textShadowColor: "rgba(0,0,0,0.75)",
+    textShadowOffset: { width: -3, height: 3 },
+    textShadowRadius: 1
+  },
+  titleText2: {
+    // fontSize: RFValue(30),
+    fontWeight: "bold",
+    color: "white",
+    fontStyle: "italic",
+    textShadowColor: "rgba(0,0,0,0.75)",
+    textShadowOffset: { width: -3, height: 3 },
+    textShadowRadius: 1
+  },
+  subHeading1: {
+    // fontSize: RFValue(20),
+    color: "#efb141",
+    fontStyle: "italic",
+    textShadowColor: "rgba(0,0,0,0.75)",
+    textShadowOffset: { width: -3, height: 3 },
+    textShadowRadius: 1
+  },
+  subHeading2: {
+    // fontSize: RFValue(20),
+    color: "white",
+    fontStyle: "italic",
+    textShadowColor: "rgba(0,0,0,0.75)",
+    textShadowOffset: { width: -3, height: 3 },
+    textShadowRadius: 1
+  },
+  framesContainer: {
+    flex: 0.2,
+    // paddingLeft: RFValue(20),
+    // paddingRight: RFValue(20),
+    // paddingTop: RFValue(30),
+    backgroundColor: "#6278e4"
+  },
+  filterImageContainer: {
+    // height: RFPercentage(8),
+    // width: RFPercentage(15),
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#e4e7f8",
+    borderRadius: 30,
+    marginRight: 20
+  },
+
+  cameraStyle: {
+    flex: 0.65
+  }
+});
